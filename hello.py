8f725a56a29c26fdfbaf97bde6008b9c6d5a9d2c
MustafaNisar2002 <131928563+MustafaNisar2002@users.noreply.github.com>
2025-09-22 11:56:27 -0400

    Example 2-2: add dynamic /user/<name> route

--- hello.py
+++ hello.py
@@ -6,5 +6,9 @@ app = Flask(__name__)            # application instance
 def index():
     return '<h1>Hello World!</h1>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
 if __name__ == '__main__':       # dev server startup
     app.run(debug=True)
