06df6bfbaef2bfddc50d15bab09c298e0bc0e0d8
MustafaNisar2002 <131928563+MustafaNisar2002@users.noreply.github.com>
2025-09-22 11:52:27 -0400

    Example 2-1: minimal Flask app with / route

---
+++ hello.py
@@ -0,0 +1,10 @@
+# hello.py  (Example 2-1)
+from flask import Flask
+app = Flask(__name__)            # application instance
+
+@app.route('/')                  # route + view function
+def index():
+    return '<h1>Hello World!</h1>'
+
+if __name__ == '__main__':       # dev server startup
+    app.run(debug=True)
